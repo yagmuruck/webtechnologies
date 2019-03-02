c8a480d9eb22209f9efce10c86b1d7995052c81c
yagmuruck <48133602+yagmuruck@users.noreply.github.com>
2019-03-02 12:13:35 +0100

    Update rgbGame.js

--- projects/rgbGame.js
+++ projects/rgbGame.js
@@ -1,57 +1,13 @@
-var randomColors = colorGenerator(4);
+
 var circles = document.getElementsByClassName("circle");
-var rightColor = numberGenerator();
 var text = document.getElementById("text");
 var startAgain = document.getElementById("start");
 
-document.getElementById("rgb").innerHTML = rightColor;
-
-startAgain.addEventListener("click", function() {
-    randomColors = colorGenerator(4);
-    rightColor = numberGenerator();
-    document.getElementById("rgb").innerHTML = rightColor;
-    document.querySelector("h1").style.backgroundColor = "RosyBrown";
-    
-    for(var i = 0; i< circles.length; i++) {
-    circles[i].style.backgroundColor = randomColors[i];
-}
-    
-});
-
-    
-for(var i = 0; i< circles.length; i++) {
-    
-    circles[i].style.backgroundColor = randomColors[i];
-    circles[i].addEventListener("click", function(){
-        
-    var chosenColor = this.style.backgroundColor;
-        
-    if(chosenColor === rightColor) {
-        text.textContent = "WOHO!";
-        changeColor(chosenColor);
-        document.querySelector("h1").style.backgroundColor = chosenColor;
-    } else {
-        this.style.backgroundColor = "black";
-        text.textContent = "Try Again!";
-    }
-       
-    });
-}
+//random color array of 4
+var randomColors = colorGenerator(4);
 
 
-function changeColor(color) {
-    for(var i = 0; i< circles.length; i++) {
-        circles[i].style.backgroundColor = color;
-    }
-}
-
-// to choose a random color from the color array every single game
-function numberGenerator() {
-    //creating a number between 0 and 3
-    var number = Math.floor(Math.random() * randomColors.length );
-    return randomColors[number];
-}
-
+//calls the function color for each circle
 function colorGenerator(number) {
     
     var colorArray = [];
@@ -61,10 +17,73 @@ function colorGenerator(number) {
     return colorArray;
 }
 
+//creates random colors for a circle
 function color() {
+    
      var red = Math.floor(Math.random() * 256);
      var green = Math.floor(Math.random() * 256);
      var blue = Math.floor(Math.random() * 256);
      
      return "rgb"+"(" + red + ", " + green + ", " + blue + ")";
+}
+
+var rightColor = rightColorGenerator();
+
+// to choose a random color from the color array 
+function rightColorGenerator() {
+    //creating a number between 0 and 3
+    var number = Math.floor(Math.random() * randomColors.length );
+    return randomColors[number];
+}
+
+document.getElementById("rgb").innerHTML = rightColor;
+
+//renew the game
+startAgain.addEventListener("click", function() {
+    
+    //new colors and new chosen color
+    randomColors = colorGenerator(4);
+    rightColor = rightColorGenerator();
+    document.getElementById("rgb").innerHTML = rightColor;
+    document.querySelector("h1").style.backgroundColor = "RosyBrown";
+    
+    //color the circles
+    for(var i = 0; i< circles.length; i++) {
+    circles[i].style.backgroundColor = randomColors[i];
+}
+    
+});
+
+    
+for(var i = 0; i< circles.length; i++) {
+    
+    //color the cirles with iterating through randomColors array
+    circles[i].style.backgroundColor = randomColors[i];
+    circles[i].addEventListener("click", function(){
+    
+    var chosenColor = this.style.backgroundColor;
+        
+    if(chosenColor === rightColor) {
+    
+        text.textContent = "WOHO!";
+        //change circles' color to the right color
+        changeColor(chosenColor);
+        //change h1 to right color
+        document.querySelector("h1").style.backgroundColor = chosenColor;
+        
+    } else {
+        
+        //make the circle "dissapear" with changing the background to black
+        this.style.backgroundColor = "black";
+        text.textContent = "Try Again!";
+    }
+       
+    });
+}
+
+//iterate through circles to change their color to the given color
+function changeColor(color) {
+    for(var i = 0; i< circles.length; i++) {
+        circles[i].style.backgroundColor = color;
+    }
 }
